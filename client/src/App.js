7692f9bd9a3e66d263d821d7c343c653a24591bd
Kyle Van Bergen <kyleavb@gmail.com>
2019-10-29 11:28:25 -0700

    updates to app.js to retrieve copy from data.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 
 function App() {
+  let state = { data: '' }
 
   axios.get('/data').then( (res) => {
-    console.log('return')
-  })
+    this.setState( {data: res.data} );
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      The Stuff
+      {state.data}
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -5,15 +5,17 @@ const config = require('./config');
 const routes = require('./routes');
 
 // Serve the static files from the React app
-app.use(express.static(path.resolve(__dirname, '../', 'client/build')));
 app.use(express.static(path.resolve(__dirname, '/data')));
+app.use(express.static(path.resolve(__dirname, '../', 'client/build')));
 
 
 app.get('/data', (req,res) =>{
+    console.log('Data request')
     res.sendFile( path.join(__dirname+'/data/data.json') );
 });
 
 app.get('/admin', (req, res) => {
+    console.log('Admin Request')
     res.send('Placeholder for admin login');
 })
 
